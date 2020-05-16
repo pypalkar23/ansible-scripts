0882e2954ff54727799cf72dcf2f13da574bce90
mandar.palkar <mandar.palkar@gupshup.io>
2020-05-16 23:55:20 +0530

    Initial Commit

---
+++ connect.sh
@@ -0,0 +1,7 @@
+ssh pr-scrapy-aws "exit" &&\
+ssh pr-master-aws "exit" &&\
+ssh pr-dashboard-aws "exit" &&\
+ssh pr-crons-aws "exit" &&\
+ssh pr-crawler-rest-aws "exit" &&\
+ssh pr-boilerpipe-aws "exit" &&\
+ssh pr-analytics-aws "exit"
